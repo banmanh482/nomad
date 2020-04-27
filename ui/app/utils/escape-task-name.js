e9abc5a4f083d722fd1a22bbcf509e57f1ee31e1
Buck Doyle <buck@hashicorp.com>
2020-04-27 10:17:04 -0500

    Add workaround to exclude emoji from task escaping
    This closes #7459.

--- ui/app/utils/escape-task-name.js
+++ ui/app/utils/escape-task-name.js
@@ -1,4 +1,19 @@
+import emojiRegex from 'emoji-regex';
+
 export default function escapeTaskName(taskName) {
+  // Store emoji character components differently to bypass escaping:
+  // "string🥳" becomes "stringUNICODE55358.UNICODE56691."
+  const taskNameWithTransformedEmoji = taskName.replace(emojiRegex(), emoji => {
+    return emoji.split('').map(char => {
+      return `UNICODE${char.charCodeAt(0)}.`;
+    }).join('');
+  });
+
   // Regular expression is taken from here: https://stackoverflow.com/a/20053121
-  return taskName.replace(/[^a-zA-Z0-9,._+@%/-]/g, '\\$&');
+  const escaped = taskNameWithTransformedEmoji.replace(/[^a-zA-Z0-9,._+@%/-]/g, '\\$&');
+
+  // Restore temporarily-transformed emoji
+  return escaped.replace(/UNICODE(\d+)./g, (match, digits) => {
+    return String.fromCharCode(digits);
+  });
 }
